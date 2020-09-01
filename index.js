608c9f59b91d3be753e0a1323dc55d79d26882c9
Ajay Kharat <iamajaykharat@gmail.com>
2020-09-01 11:56:16 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,71 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+//Fetch laptop data from json file
+const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
+const laptopData = JSON.parse(json);
+
+//Server
+const server = http.createServer((req, res)=>{
+
+  const pathName = url.parse(req.url, true).pathname;
+  const id = url.parse(req.url, true).query.id;
+  
+  //PRODUCT OVERVIEW
+  if(pathName === '/products' || pathName ==='/'){
+    res.writeHead(200, { 'Content-type': 'text/html' });
+
+    fs.readFile(`${__dirname}/templates/template-overview.html`, 'utf-8', (err, data) => {
+      let overviewOutput = data;
+
+      fs.readFile(`${__dirname}/templates/template-card.html`, 'utf-8', (err, data) => {
+        const cardOutput = laptopData.map(el => replacePlaceholder(data, el)).join('');
+        overviewOutput = overviewOutput.replace(/{%CARDS%}/g, cardOutput);
+        res.end(overviewOutput);
+      });
+    });
+  }
+
+  //LAPTOP DETAILS
+  else if(pathName === '/laptop' && id < laptopData.length){
+    res.writeHead(200, { 'Content-type': 'text/html' });
+
+    fs.readFile(`${__dirname}/templates/template-laptop.html`, 'utf-8', (err, data) => {
+      const laptop = laptopData[id];
+      const output = replacePlaceholder(data, laptop);
+      res.end(output);
+    });
+  }
+
+  //IMAGE LOADS
+  else if((/\.(jpg|jpeg|png|gif)$/i).test(pathName)){
+    fs.readFile(`${__dirname}/data/img${pathName}`, (err, data)=>{
+      res.writeHead(200, { 'Content-type': 'image/jpg' });
+      res.end(data);
+    });
+  }
+
+  //URL NOT FOUND
+  else{
+    res.writeHead(404, { 'Content-type': 'text/html' });
+    res.end('Opps! URL you have requested is not present!');
+  } 
+});
+
+server.listen(1337, '127.0.0.1',()=>{
+  console.log('Listening from server....');
+});
+
+function replacePlaceholder(originalHtml, laptop){
+  let output = originalHtml.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+  output = output.replace(/{%PRICE%}/g, laptop.price);
+  output = output.replace(/{%CPU%}/g, laptop.cpu);
+  output = output.replace(/{%SCREEN%}/g, laptop.screen);
+  output = output.replace(/{%STORAGE%}/g, laptop.storage);
+  output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+  output = output.replace(/{%RAM%}/g, laptop.ram);
+  output = output.replace(/{%IMAGE%}/g, laptop.image);
+  output = output.replace(/{%ID%}/g, laptop.id);
+  return output;
+}
